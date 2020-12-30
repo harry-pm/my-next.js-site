fc5fb41fe100135bb5093b125628468c06c37b25
harry-pm <harrypenderedmazer@gmail.com>
2020-12-30 13:01:25 +0100

    completed nextJS tutorial for this section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a92e5c6693fd0c8d2565";
+/******/ 	var hotCurrentHash = "d25beab53b976442582f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
