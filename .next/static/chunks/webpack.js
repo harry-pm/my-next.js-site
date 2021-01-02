ed980f9f6c74d0304fc36db3c70ac0dfc9e515e8
harry-pm <harrypenderedmazer@gmail.com>
2021-01-02 18:11:47 +0100

    readme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e3c585afa02b05ab1df";
+/******/ 	var hotCurrentHash = "be604d41e2232510e815";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
